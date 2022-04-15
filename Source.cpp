2b18b6786b364e3f9508a25b8a9e4b7a7e7938d9
MarizovAlexey6211 <mar.karnage@outlook.com>
2022-04-15 17:41:47 +0400

    add node structure

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
+template <typename T>
+struct Node {
+    
+    T data;
+    Node* left;
+    Node* right;
+};
 int main(int argc, char* argv[]) {
     return 0;
 }
