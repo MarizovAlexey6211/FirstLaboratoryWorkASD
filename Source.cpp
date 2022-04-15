191ec849f04608f44e05d2ffab1e33f6cfeb9f3d
MarizovAlexey6211 <mar.karnage@outlook.com>
2022-04-15 19:06:49 +0400

    print method

--- Source.cpp
+++ Source.cpp
@@ -6,6 +6,17 @@ struct Node {
     T data;
     Node* left;
     Node* right;
+public:
+    void inorder(Node* r)
+    {
+        if (r == NULL) {
+            return;
+        }
+        inorder(r->left);
+        cout << r->data << " ";
+        inorder(r->right);
+    }
+
 };
 int main(int argc, char* argv[]) {
     return 0;
