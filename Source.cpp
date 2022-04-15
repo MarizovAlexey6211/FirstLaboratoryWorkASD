dfa75f7b62cfadc2a9c54fc86503a0f4021987b5
MarizovAlexey6211 <mar.karnage@outlook.com>
2022-04-15 19:33:19 +0400

    find element

--- Source.cpp
+++ Source.cpp
@@ -17,6 +17,14 @@ public:
         inorder(r->right);
     }
 
+    int containsNode(Node* r, T d)
+    {
+        if (r == NULL) {
+            return 0;
+        }
+        int x = r->data == d ? 1 : 0;
+        return x | containsNode(r->left, d) | containsNode(r->right, d);
+    }
 };
 int main(int argc, char* argv[]) {
     return 0;
