ae4e744f212eb8ca9a4faa66752e35f5ffcf23b3
MarizovAlexey6211 <mar.karnage@outlook.com>
2022-04-15 20:32:23 +0400

    insert element

--- Source.cpp
+++ Source.cpp
@@ -25,6 +25,25 @@ public:
         int x = r->data == d ? 1 : 0;
         return x | containsNode(r->left, d) | containsNode(r->right, d);
     }
+    Node* insert(Node* r, T d)
+    {
+        if (r == NULL) {
+            Node<T>* tmp = new Node<T>;
+            tmp->data = d;
+            tmp->left = tmp->right = NULL;
+            return tmp;
+        }
+        if (d < r->data) {
+            r->left = insert(r->left, d);
+            return r;
+        }
+        else if (d > r->data) {
+            r->right = insert(r->right, d);
+            return r;
+        }
+        else
+            return r;
+    }
 };
 int main(int argc, char* argv[]) {
     return 0;
